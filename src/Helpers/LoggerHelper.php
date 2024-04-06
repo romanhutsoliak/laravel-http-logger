f109fcc64c82ec96b3acf57fb28dc5891c9650e7
Roman Hutsoliak <roman.hutsoliak@gmail.com>
2024-04-06 14:56:14 +0300

    http_logger enabled config amendment2

--- src/Helpers/LoggerHelper.php
+++ src/Helpers/LoggerHelper.php
@@ -12,14 +12,17 @@ class LoggerHelper
     public static function isServiceEnabled(): bool
     {
         $isEnabled = false;
+
         if (app()->environment('local')) {
             $isEnabled = true;
         }
+
         // can be overwritted in services.http_logger_enabled as true or false for
         $servicesHttp_logger_enabled = config('services.http_logger.enabled');
         if (!is_null($servicesHttp_logger_enabled) && is_bool($servicesHttp_logger_enabled)) {
             $isEnabled = $servicesHttp_logger_enabled;
         }
+
         return $isEnabled;
     }
 }
